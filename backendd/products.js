643256ef41900c006795ea913487f7b9d7dc3de0
lolashot <ololadeshot@gmail.com>
2022-10-30 14:30:26 +0100

    cart functionalities done

---
+++ backendd/products.js
@@ -0,0 +1,52 @@
+const products = [
+    {
+      id: 1,
+      name: "iphone 12 Pro",
+      desc: "5.3-inch display",
+      price: "400",
+      image: "https://res.cloudinary.com/detpgfyff/image/upload/v1665644001/onlineshop/phone2_ikzclm.jpg",
+    },
+    {
+      id: 2,
+      name: "shoe S",
+      desc: "adidas size 45",
+      price: "600",
+      image: "https://res.cloudinary.com/detpgfyff/image/upload/v1665643990/onlineshop/shoes2_iehp8j.jpg",
+    },
+    {
+      id: 3,
+      name: "t-shirt",
+      desc: "tony shirts size30-45",
+      price: "800",
+      image: "https://res.cloudinary.com/detpgfyff/image/upload/v1665644018/onlineshop/t_shirts_tokvdi.jpg",
+    },
+    {
+      id: 4,
+      name: "Shoe",
+      desc: "tony shoes size30-45",
+      price: "800",
+      image: "https://res.cloudinary.com/detpgfyff/image/upload/v1665643969/onlineshop/shoes_1_y2uuqg.jpg",
+    },
+    {
+      id: 5,
+      name: "shoe",
+      desc: "tony shoes size30-45",
+      price: "800",
+      image: "https://res.cloudinary.com/detpgfyff/image/upload/v1665644007/onlineshop/shoes_s7fzvp.jpg",
+    },
+    {
+      id: 6,
+      name: "Samsung",
+      desc: "5-10 inch display",
+      price: "800",
+      image: "https://res.cloudinary.com/detpgfyff/image/upload/v1665643952/onlineshop/phone1_f1wotb.jpg",
+    },
+    {
+      id: 7,
+      name: "Shirt",
+      desc: "tony shirts size30-45",
+      price: "800",
+      image: "https://res.cloudinary.com/detpgfyff/image/upload/v1665643978/onlineshop/shirts_nk1e0l.jpg",
+    },
+  ];
+  module.exports = products;
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+import {Link} from 'react-router-dom';
+const NavBar = () => {
+  const {cartTotalQuantity} = useSelector(state => state.cart)
+    return ( 
+    <nav className="nav-bar">
+        <Link to="/">
+            <h2>Topklazzic</h2>
+            </Link>
+            <Link to="/cart">
+    <div className="shop-bag">
+    <svg xmlns="http://www.w3.org/2000/svg" 
+    width="35"
+     height="35" 
+     fill="currentColor" 
+     class="bi bi-cart-fill" 
+     viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg>
+<span className="bag-quantity"> 
+<span>{cartTotalQuantity}</span>
+</span>
+    </div>
+    </Link>
+  </nav>
+     );
+}
+ 
+export default NavBar;
